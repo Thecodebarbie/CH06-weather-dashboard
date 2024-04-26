72d6540b5f8dbd88977a568879628ce8341a5f90
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-26 14:29:26 -0400

    created card for current-weather and added Titlecase to capitalize first Letter of city name.
    saved city name to local storage
    
    display weather forecast upon click event.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,43 @@
 const cityInput = document.getElementById('input-city');
 const searchBtn = document.getElementById('btn-search');
-const weatherCardsEl = document.getElementById('weather-cards')
+const weatherCardsEl = document.getElementById('weather-cards');
+const detailsEL = document.getElementById('details');
+const currentWeatherEl = document.getElementById('current-weather');
+
 
 const API_KEY = '0da1455d9ed9eed2bab607b8c3dbad8a'; // API key for OpenWeatherMap API
 
-const createWeatherCard = (weatherItem) => {
-    const days = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday','Saturday','Sunday'];
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+const createCurrentWeatherCard = (weatherItem) => {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const date = new Date(weatherItem.dt_txt.split(' ')[0]);
-    const dayOfWeek = days [date.getDay()];
+    const dayOfWeek = days[date.getDay()];
+    return `
+    <section id="currentDay">
+        <h2> ${toTitleCase(cityInput.value)} (${dayOfWeek})</h2>
+        <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+        <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+        <h6>Humidity:  ${weatherItem.main.humidity}%</h6>
+    </section>
+    <figure class="icon">
+      <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="">
+      <h4>Moderate Rain</h4>
+    </figure>`
+}
+
+
+const createWeatherCard = (weatherItem) => {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const date = new Date(weatherItem.dt_txt.split(' ')[0]);
+    const dayOfWeek = days[date.getDay()];
 
     return `<li class="card">
     <h3 class="day-of-week">${dayOfWeek}</h3>
@@ -18,8 +48,6 @@ const createWeatherCard = (weatherItem) => {
     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
     </li>`;
 
-
-
 }
 
 
@@ -29,17 +57,22 @@ const getWeatherDetails = (cityName, lat, lon) => {
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         // Filter the forecasts to get only one forecast per day
-        console.log(data.list)
-        const uniqueForecastDays =[];
+        console.log(data.city.name)
+        const savedCitiesArray = JSON.parse(localStorage.getItem('saved-city')) || []
+        savedCitiesArray.push(data.city.name)
+        localStorage.setItem('saved-city', JSON.stringify(savedCitiesArray));
+        const uniqueForecastDays = [];
         const fiveDayForecast = data.list.filter(forecast => {
             const forecastDate = new Date(forecast.dt_txt).getDate();
-            if(!uniqueForecastDays.includes(forecastDate)) {
+            if (!uniqueForecastDays.includes(forecastDate)) {
                 return uniqueForecastDays.push(forecastDate);
             }
         });
+        var currentWeather = fiveDayForecast[0]
+        detailsEL.innerHTML = createCurrentWeatherCard(currentWeather)
         fiveDayForecast.shift()
         console.log(fiveDayForecast);
-        
+
         // Clear previous weather data from the weather cards element
         weatherCardsEl.innerHTML = '';
 
@@ -56,13 +89,15 @@ const getWeatherDetails = (cityName, lat, lon) => {
 
 const fetchCityCoordinates = () => {
     const cityName = cityInput.value.trim(); //Get user entered city name and remove extras. 
-    if(!cityName) return; // Return if cityName is empty.
+    if (!cityName) return; // Return if cityName is empty.
     const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
-    fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
-        if(!data.length) return alert(`No coordinates found for ${cityName}`)
-        const {name, lat, lon} = data[0];
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`)
+        const { name, lat, lon } = data[0];
         getWeatherDetails(name, lat, lon);
+
+        currentWeatherEl.style.display = "block"
     }).catch(() => {
         console.log('An error occurred while fetching the coordinates!');
     });
