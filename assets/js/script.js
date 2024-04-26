c1202c60b26dbde4c45ec13678d90dd9e614e078
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-26 13:18:09 -0400

    weather card creation to display day of the week and weather type, and fetch only 5 days of forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 const cityInput = document.getElementById('input-city');
 const searchBtn = document.getElementById('btn-search');
+const weatherCardsEl = document.getElementById('weather-cards')
 
 const API_KEY = '0da1455d9ed9eed2bab607b8c3dbad8a'; // API key for OpenWeatherMap API
+
+const createWeatherCard = (weatherItem) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday','Saturday'];
+    const date = new Date(weatherItem.dt_txt.split(' ')[0]);
+    const dayOfWeek = days [date.getDay()];
+
+    return `<li class="card">
+    <h3 class="day-of-week">${dayOfWeek}</h3>
+    <img src= "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+    <h4>Weather: ${weatherItem.weather[0].main}</h4>
+    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+    <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+    </li>`;
+
+
+
+}
+
+
+
 const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&&appid=${API_KEY}`;
 
@@ -16,9 +38,18 @@ const getWeatherDetails = (cityName, lat, lon) => {
         });
 
         console.log(fiveDayForecast);
+        
+        // Clear previous weather data from the weather cards element
+        weatherCardsEl.innerHTML = '';
 
-    }).catch(() => {
+        // Iterate over each weather item and create weather cards
+        fiveDayForecast.forEach(weatherItem => {
+            weatherCardsEl.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
+        });
+
+    }).catch((error) => {
         alert('An error occurred while fetching the weather forecast!');
+        console.log(error)
     });
 }
 
@@ -32,7 +63,7 @@ const fetchCityCoordinates = () => {
         const {name, lat, lon} = data[0];
         getWeatherDetails(name, lat, lon);
     }).catch(() => {
-        alert('An error occurred while fetching the coordinates!');
+        console.log('An error occurred while fetching the coordinates!');
     });
 }
 
